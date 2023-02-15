a335ff5e5b4b6c8a0871c37627b48b0dc960213b
Gabriel-mdv <mpuhwegabriel@gmail.com>
2023-02-15 17:27:16 +0200

    all changes

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const menu = document.getElementsByClassName('menu')[0]
 const navlist = document.getElementsByClassName(' navlist')[0]
 
@@ -28,4 +30,91 @@ function init(){
     
 }
 
-init()
+
+
+
+
+// ---- slider ------------------ 
+const buttons = document.querySelectorAll("[data-carousel-Button]")
+console.log("hello")
+console.log(buttons)
+buttons.forEach((button) => {
+    button.addEventListener('click', ()=> {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]")
+
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+
+        if (newIndex < 0) newIndex = slides.children.length - 1 
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+
+        delete activeSlide.dataset.active
+
+
+    } )
+    
+});
+
+
+// --------- getting the submitted data ----- 
+const name1 = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector("#message");
+let count = 0;
+    
+
+
+function sendMessage(event){
+    event.preventDefault();
+
+    validateInputs();
+
+    count % 3 === 0 ? (name1.value = "", email.value= message.value = ""):(
+        console.log("not all procceeced")
+    )
+}
+
+const emailIsValid = value => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(value).toLocaleLowerCase());
+}
+
+const validateInputs = () => {
+    const nameValue = name1.value.trim();
+    const emailValue = email.value.trim();
+    const messageValue = message.value.trim();
+
+    
+    nameValue ? (success(name1)): error(name1, "*Enter your name please!");
+    emailValue && emailIsValid(emailValue) ? (success(email)): error(email, "*Please enter a valid email!");
+    messageValue ? (success(message)): error(message, "*Please enter a message")
+}
+
+
+function error(element, message){
+    element.value = "";
+    const inputControl = element.parentElement;
+    const displayError = inputControl.querySelector('.output')
+
+    displayError.classList.remove('success');
+    displayError.classList.add('error')  
+    displayError.innerText = message
+
+}
+
+function success(element){
+    count ++;
+
+    const inputControl = element.parentElement;
+    const displayError = inputControl.querySelector('.output')
+
+    displayError.classList.remove('error');
+    displayError.classList.add('success');
+    console.log(element.value)    
+}
